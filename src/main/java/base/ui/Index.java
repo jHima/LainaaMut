8fd6ef5e54a1731407fb5d1cedae9ab77e724549
jHima <jutta.himanen@gmail.com>
2020-09-27 19:34:45 +0300

    css

--- src/main/java/base/ui/Index.java
+++ src/main/java/base/ui/Index.java
@@ -2,13 +2,16 @@ package base.ui;
 
 import java.util.UUID;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import base.model.Tavara;
 
@@ -18,7 +21,14 @@ public class Index extends WebPage {
 
 	public Index() {
 		
+		
 		add(header = new Header("header"));
+		
+		add(new Label("simpleLabel", "Tekstiä tässä"));
+		
+		//add(new Label("simpleLabel"));
+		
+		//label.add(new AttributeModifier("style", "color:red"));
 		
 		add(new Image("image4", new Model<>("urheiluvalineet.jpeg")));
 		
