61ca53babeee7bb119cfeb590e2115dae8d433d7
jHima <jutta.himanen@gmail.com>
2020-10-25 14:31:38 +0200

    käytäjä muokattu

--- src/main/java/base/dao/KayttajaDao.java
+++ src/main/java/base/dao/KayttajaDao.java
@@ -1,5 +1,36 @@
 package base.dao;
 
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class KayttajaDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public boolean tarkistaSalasana(String nimi, String salasana) {
+		try (Session session = sessionFactory.openSession()) {
+
+			try {
+				String hql = "from Kayttaja where nimi = :nimi and salasana = :salasana";
+				Query query = session.createQuery(hql);
+				query.setParameter("nimi", nimi);
+				query.setParameter("salasana", salasana);
+				
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+		}
+
+		return true;
+	}
 
 }
